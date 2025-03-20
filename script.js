2100a773d9f1fdcedfeda76415039d53e381f657
A. Taylor <alecia.taylor0@gmail.com>
2025-03-20 01:26:19 +0000

    Implement caesarCipher function to perform character shifting based on the provided shift value

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-function caesarCipher(string, shift){
-    return string;
-}
+function caesarCipher(string, shift) {
+    return string.replace(/[a-zA-Z]/g, function(c) {
+      const base = c < 'a' ? 65 : 97;
+      return String.fromCharCode((c.charCodeAt(0) - base + shift) % 26 + base);
+    });
+  }
